f431db2e079f82af2ad0b4e58eb23c493a7a7598
Leo Yang <0.yxyun@gmail.com>
2023-12-30 18:47:05 -0800

    expose style

--- src/index-pkg.ts
+++ src/index-pkg.ts
@@ -1,3 +1,4 @@
+import "virtual:uno.css";
 import ASheet from "./components/sheet.vue";
 
 export { ASheet };
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [UnoCSS(), vue()],
   build: {
     target: "esnext",
     lib: {
