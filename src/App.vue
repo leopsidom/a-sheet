c158abe3606b7fa8d7d2e51339325ccf3ccc680c
Leo Yang <0.yxyun@gmail.com>
2023-12-31 19:16:48 -0800

    support multi index display

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <sheet :data="data" :columns="columns" :index="index"></sheet>
+  <sheet :data="data2" :columns="columns2" :index="index2"></sheet>
 </template>
 
 <script setup lang="ts">
@@ -18,6 +19,30 @@ const data = [
 
 const columns = ["col1", "col2", "col3", "col4"];
 const index = [0, 1, 2, 3];
+
+const data2 = [
+  ["a", "b", "m", "n"],
+  [
+    "c",
+    "d                     3287233829819218728172191281972872837",
+    "d",
+    "e",
+  ],
+  ["a", "b", "m", "n"],
+];
+
+const columns2 = [
+  ["col1", "col11"],
+  ["col2", "col22"],
+  ["col3", "col22"],
+  ["col4", "col22"],
+];
+const index2 = [
+  [0, 0],
+  [1, 0],
+  [2, 0],
+  [3, 0],
+];
 </script>
 
 <style scoped></style>
--- src/components/sheet.vue
+++ src/components/sheet.vue
@@ -1,30 +1,59 @@
 <template>
-  <div>
-    <div
-      v-if="columns.length > 0"
-      class="flex border-y-slate-500 border-y border-y-solid w-fit"
-    >
-      <div class="w-5 px-2 py-px border-x-slate-500 border-x border-x-solid">
-        {{ "" }}
-      </div>
+  <div
+    class="
+      border-l-slate-400
+      border-l
+      border-l-solid
+      border-t-slate-400
+      border-t
+      border-t-solid
+      w-fit
+    "
+  >
+    <div v-if="columns.length > 0" class="flex">
       <div
-        v-for="(val, colIndex) in columns"
-        :key="colIndex"
         class="
-          border-r-slate-500 border-r border-r-solid
-          w-20
           px-2
           py-px
-          overflow-hidden
+          border-r-slate-400
+          border-r
+          border-r-solid
+          border-b-slate-400
+          border-b
+          border-b-solid
+          box-border
         "
-      >
-        {{ val }}
+        :class="size"
+      />
+      <div class="flex-col w-fit">
+        <div v-for="(levelColumns, level) in columns" :key="level" class="flex">
+          <div
+            v-for="(val, colIndex) in levelColumns"
+            :key="colIndex"
+            class="
+              border-r-slate-400
+              border-r
+              border-r-solid
+              border-b-slate-400
+              border-b
+              border-b-solid
+              box-border
+              w-20
+              h-5
+              px-2
+              py-px
+              overflow-hidden
+            "
+          >
+            {{ val }}
+          </div>
+        </div>
       </div>
     </div>
     <div
       v-for="(row, rowIndex) in props.data"
       :key="rowIndex"
-      class="flex border-b border-b-slate-300 border-b-solid w-fit"
+      class="flex w-fit"
       :class="
         rowIndex === 0 && columns.length === 0
           ? 'border-t border-t-slate-300 border-t-solid'
@@ -33,24 +62,39 @@
     >
       <div
         class="
-          border-x-slate-500 border-x border-x-solid
           w-5
+          flex
+          overflow-hidden
+          box-border
+          border-r-slate-400
+          border-r
+          border-r-solid
+          border-b
+          border-b-slate-400
+          border-b-solid
           px-2
           py-px
-          overflow-hidden
         "
+        v-for="(levelIndex, level) in index[rowIndex]"
+        :key="level"
       >
-        {{ index[rowIndex] }}
+        {{ levelIndex }}
       </div>
       <div
         v-for="(val, colIndex) in row"
         :key="colIndex"
         class="
-          border-r-slate-300 border-r border-r-solid
+          border-r-slate-300
+          border-r
+          border-r-solid
+          border-b
+          border-b-slate-300
+          border-b-solid
           w-20
           px-2
           py-px
           overflow-hidden
+          box-border
         "
       >
         {{ val }}
@@ -60,12 +104,49 @@
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from "vue";
+type IndexType = (string | number)[][] | (string | number)[];
 const props =
   defineProps<{
     data: string[][];
-    columns: string[];
-    index: (string | number)[];
+    columns: IndexType;
+    index: IndexType;
   }>();
+
+const widths = ref<number[]>([]);
+
+function normalizeIndex(
+  index: IndexType,
+  transpose?: boolean
+): (string | number)[][] {
+  if (index.length === 0) return [[]];
+  if (index[0] instanceof Array) {
+    if (transpose) {
+      return index as (string | number)[][];
+    } else {
+      const result = index[0].map((i) => [i]);
+      for (let i = 1; i < index.length; i++) {
+        const iThIndex = index[i] as (string | number)[];
+        for (let j = 0; j < iThIndex.length; j++) {
+          result[j].push(iThIndex[j]);
+        }
+      }
+      return result;
+    }
+  } else {
+    return (transpose ? index.map((i) => [i]) : [index]) as (
+      | string
+      | number
+    )[][];
+  }
+}
+
+const columns = computed(() => normalizeIndex(props.columns, false));
+const index = computed(() => normalizeIndex(props.index, true));
+
+const size = computed(
+  () => `w-${index.value[0].length * 5} h-${columns.value.length * 5}`
+);
 </script>
 
 <style scoped></style>
